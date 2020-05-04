1735db6fbda3221446e9d1375bffc90d89ac1a93
mateo-balza <57110825+matking2001@users.noreply.github.com>
2020-05-04 11:08:41 -0500

    Add files via upload

---
+++ insertar.php
@@ -0,0 +1,36 @@
+<?php 
+
+    include("conexion.php");
+
+    if ($conect){
+        echo "Conexion exitosa";
+    }else{
+        echo "Error con la conexion";
+    }
+
+    if (isset($_POST['validar'])){
+        if(isset($_POST['nombre1']) && !empty($_POST['nombre1']) && isset($_POST['apellido1']) && !empty($_POST['apellido2'])
+        && isset($_POST['correo']) && !empty($_POST['correo']) && isset($_POST['pw']) && !empty($_POST['pw']) && isset($_POST['fecha']) 
+        && !empty($_POST['fecha']) && isset($_POST['sexo']) && !empty($_POST['sexo'])){
+
+            $nombre1= trim($_POST['nombre1']);
+            $nombre2= trim($_POST['nombre2']);
+            $apellido1= trim($_POST['apellido1']);
+            $apellido2= trim($_POST['apellido2']);
+            $correo= trim($_POST['correo']);
+            $pw= trim($_POST['pw']);
+            $sexo= trim($_POST['sexo']);
+            $fecha= trim($_POST['fecha']);
+            $datos= "INSERT INTO 'registro' ('Nombre1','nombre2','apellido1','apellido2','correo','contraseña','sexo','fecha_Nac') VALUES 
+            ('$nombre1'.'$nombre2','$apellido1','$apellido2','$correo','$pw','$sexo','$fecha')";
+
+            $reg= mysqli_query($conect,$datos);
+
+            echo "datos guardados correctamente"
+
+        }else{
+            echo "se produjo un error"
+        }
+    }
+
+?>
