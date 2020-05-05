fab97550fb3a2aabf5dbce33ed223775db380fdb
Mateo balza <balzamateo5@gmail.com>
2020-05-05 17:08:42 -0500

    Se modifico el h1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <div class="formulario">
         <form  method="POST">
-            <h1><img class="logo" src="https://www.tekla.io/wp-content/uploads/2019/09/logo-1162901_1920.png"></h1>
+            <h1 class="modal-dialog text-center" >Form Registro</h1>
             <input class="datos" type="text" name="nombre1" placeholder="Primer nombre">
             <input class="datos" type="text" name="nombre2" placeholder="segundo nombre">
             <input class="datos" type="text" name="apellido1" placeholder="Primer apellido">
@@ -28,11 +28,11 @@
             </div>
 
             <div class="ch">
-                <input type="checkbox" class="ck" name="radio" id=""><label for="" class="l-radio">Acepta <a href="">terminos y condciones?</a> </label>
+                <input type="checkbox" class="ck" name="radio" id=""><label for="" class="l-radio">Acepta <a href="https://www.masquenegocio.com/2016/05/16/ecommerce-pagina-web/">terminos y condciones?</a> </label>
             </div> 
 
             <br>
-            <input class="btn-primary boton" type="submit" name="validar" value="Registrar" >
+            <input class="btn-danger boton" type="submit" name="validar" value="Registrar" >
         </form>
     </div>
 
